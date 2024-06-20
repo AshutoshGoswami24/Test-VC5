bd07c8c1441c47c3c934749ced691d3ad45708c9
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-06-20 12:21:06 +0530

    Add files via upload

---
+++ plugins/start.py
@@ -0,0 +1,100 @@
+import asyncio
+import shutil
+import humanize
+from time import sleep
+from config import Config
+from script import Txt
+from helper.database import db
+from pyrogram.errors import FloodWait
+from pyrogram import Client, filters, enums
+from .check_user_status import handle_user_status
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+
+@Client.on_message((filters.private | filters.group))
+async def _(bot: Client, cmd: Message):
+    await handle_user_status(bot, cmd)
+
+@Client.on_message((filters.private | filters.group) & filters.command('start'))
+async def Handle_StartMsg(bot:Client, msg:Message):
+
+    Snowdev = await msg.reply_text(text= '**Please Wait...**', reply_to_message_id=msg.id)
+
+    if msg.chat.type == enums.ChatType.SUPERGROUP and not await db.is_user_exist(msg.from_user.id):
+        botusername = await bot.get_me()
+        btn = [
+            [InlineKeyboardButton(text='⚡ BOT PM', url=f'https://t.me/{botusername.username}')],
+            [InlineKeyboardButton(text='❤️ 𝗠𝗮𝗶𝗻 𝗖𝗵𝗮𝗻𝗻𝗮𝗹 ❤️', url='https://t.me/PandaWep')]
+        ]
+
+        await Snowdev.edit(text=Txt.GROUP_START_MSG.format(msg.from_user.mention), reply_markup=InlineKeyboardMarkup(btn))
+    
+    else:
+        btn = [
+            [InlineKeyboardButton(text='❗ Hᴇʟᴘ', callback_data='help'), InlineKeyboardButton(text='🌨️ Aʙᴏᴜᴛ', callback_data='about')],
+            [InlineKeyboardButton(text='😁 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ 😁', url='https://t.me/PandaWepChat'), InlineKeyboardButton(text='❤️ 𝗠𝗮𝗶𝗻 𝗖𝗵𝗮𝗻𝗻𝗮𝗹 ❤️ ', url='https://t.me/PandaWep')]
+        ]
+
+        if Config.START_PIC:
+            await Snowdev.delete()
+            await msg.reply_photo(photo=Config.START_PIC, caption=Txt.PRIVATE_START_MSG.format(msg.from_user.mention), reply_markup=InlineKeyboardMarkup(btn), reply_to_message_id=msg.id)
+        else:
+            await Snowdev.delete()
+            await msg.reply_text(text=Txt.PRIVATE_START_MSG.format(msg.from_user.mention), reply_markup=InlineKeyboardMarkup(btn), reply_to_message_id=msg.id)
+            
+LOG_CHANNEL = "-1002135731047"
+
+@Client.on_message((filters.private | filters.group) & (filters.document | filters.audio | filters.video))
+async def Files_Option(bot:Client, message:Message):
+    
+    SnowDev = await message.reply_text(text='**Please Wait**', reply_to_message_id=message.id)
+    
+
+    if message.chat.type == enums.ChatType.SUPERGROUP and not await db.is_user_exist(message.from_user.id):
+        botusername = await bot.get_me()
+        btn = [
+            [InlineKeyboardButton(text='⚡ BOT PM', url=f'https://t.me/{botusername.username}')],
+            [InlineKeyboardButton(text='❤️ 𝗠𝗮𝗶𝗻 𝗖𝗵𝗮𝗻𝗻𝗮𝗹 ❤️ ', url='https://t.me/PandaWep')]
+        ]
+
+        return await SnowDev.edit(text=Txt.GROUP_START_MSG.format(message.from_user.mention), reply_markup=InlineKeyboardMarkup(btn))
+        
+    file = getattr(message, message.media.value)
+    filename = file.file_name
+    filesize = humanize.naturalsize(file.file_size)
+
+
+    try:
+        await message.copy(chat_id=LOG_CHANNEL)
+        text = f"""**__What do you want me to do with this file.?__**\n\n**File Name** :- `{filename}`\n\n**File Size** :- `{filesize}`"""
+
+        buttons = [[InlineKeyboardButton("Rᴇɴᴀᴍᴇ 📝", callback_data=f"rename-{message.from_user.id}")],
+                   [InlineKeyboardButton("Cᴏᴍᴘʀᴇss 🗜️", callback_data=f"compress-{message.from_user.id}")]]
+        await SnowDev.edit(text=text, reply_markup=InlineKeyboardMarkup(buttons))
+        
+    except FloodWait as e:
+        
+        floodmsg = await message.reply_text(f"**😥 Pʟᴇᴀsᴇ Wᴀɪᴛ ᴅᴏɴ'ᴛ ᴅᴏ ғʟᴏᴏᴅɪɴɢ ᴡᴀɪᴛ ғᴏʀ {e.value} Sᴇᴄᴄᴏɴᴅs**", reply_to_message_id=message.id)
+        await sleep(e.value)
+        await floodmsg.delete()
+
+        text = f"""**__What do you want me to do with this file.?__**\n\n**File Name** :- `{filename}`\n\n**File Size** :- `{filesize}`"""
+        buttons = [[InlineKeyboardButton("Rᴇɴᴀᴍᴇ 📝", callback_data=f"rename-{message.from_user.id}")],
+                   [InlineKeyboardButton("Cᴏᴍᴘʀᴇss 🗜️", callback_data=f"compress-{message.from_user.id}")]]
+        await SnowDev.edit(text=text, reply_markup=InlineKeyboardMarkup(buttons))
+
+    except Exception as e:
+        print(e)
+
+@Client.on_message((filters.private | filters.group) & filters.command('cancel'))
+async def cancel_process(bot:Client, message:Message):
+    
+    try:
+        shutil.rmtree(f"encode/{message.from_user.id}")
+        shutil.rmtree(f"ffmpeg/{message.from_user.id}")
+        shutil.rmtree(f"Renames/{message.from_user.id}")
+        shutil.rmtree(f"Metadata/{message.from_user.id}")
+        shutil.rmtree(f"Screenshot_Generation/{message.from_user.id}")
+        
+        return await message.reply_text(text="**Canceled All On Going Processes ✅**")
+    except BaseException:
+        pass
