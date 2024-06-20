52df460220ee61a7a8eed899a1ece6248ab58fd4
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-06-20 12:26:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,14 +8,14 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID = os.environ.get("API_ID", "29917436")  # ⚠️ Required
-    API_HASH = os.environ.get("API_HASH", "4a926822b076a086a167fe8f2701d3e9")  # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7226766470:AAEpx_0WhehNcUKu_8sBfiYmbb5tpZteU50")  # ⚠️ Required
+    API_ID = os.environ.get("API_ID", "")  # ⚠️ Required
+    API_HASH = os.environ.get("API_HASH", "")  # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")  # ⚠️ Required
     FORCE_SUB = os.environ.get('FORCE_SUB', '-1002116868480')  # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(FORCE_SUB) else None
     AUTH_USERS = [7226766470, 7062828064, 6537859]
     # database config
-    DB_URL = os.environ.get("DB_URL", "mongodb+srv://botzashu:LjkXI1JoztDQiQlr@cluster0.38ague0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # ⚠️ Required
+    DB_URL = os.environ.get("DB_URL", "")  # ⚠️ Required
     DB_NAME = os.environ.get("DB_NAME", "Cluster0") 
 
     # Other Configs 
